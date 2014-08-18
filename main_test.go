b4db140e7527d75af12cf3e290590bfd5339fc41
Max Brunsfeld <mbrunsfeld@pivotallabs.com>
2014-08-18 15:13:32 -0700

    Add test coverage to main test
    Signed-off-by: Ted Young <tyoung@pivotallabs.com>

--- main_test.go
+++ main_test.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/cloudfoundry/gunk/timeprovider"
+	"github.com/cloudfoundry/storeadapter"
 	"github.com/cloudfoundry/storeadapter/storerunner/etcdstorerunner"
 	"github.com/cloudfoundry/storeadapter/test_helpers"
 	. "github.com/onsi/ginkgo"
@@ -17,6 +18,7 @@ import (
 	"github.com/cloudfoundry-incubator/converger/converger_runner"
 	Bbs "github.com/cloudfoundry-incubator/runtime-schema/bbs"
 	"github.com/cloudfoundry-incubator/runtime-schema/bbs/services_bbs"
+	"github.com/cloudfoundry-incubator/runtime-schema/bbs/shared"
 	"github.com/cloudfoundry-incubator/runtime-schema/models"
 )
 
@@ -29,6 +31,10 @@ var _ = Describe("Main", func() {
 		fileServerPresence services_bbs.Presence
 
 		taskKickInterval = 1 * time.Second
+
+		etcdClient storeadapter.StoreAdapter
+
+		heartbeatInterval = time.Second
 	)
 
 	BeforeSuite(func() {
@@ -36,7 +42,8 @@ var _ = Describe("Main", func() {
 		etcdCluster := fmt.Sprintf("http://127.0.0.1:%d", etcdPort)
 		etcdRunner = etcdstorerunner.NewETCDClusterRunner(etcdPort, 1)
 
-		bbs = Bbs.NewBBS(etcdRunner.Adapter(), timeprovider.NewTimeProvider(), lagertest.NewTestLogger("test"))
+		etcdClient = etcdRunner.Adapter()
+		bbs = Bbs.NewBBS(etcdClient, timeprovider.NewTimeProvider(), lagertest.NewTestLogger("test"))
 
 		convergerBinPath, err := gexec.Build("github.com/cloudfoundry-incubator/converger", "-race")
 		Ω(err).ShouldNot(HaveOccurred())
@@ -45,22 +52,29 @@ var _ = Describe("Main", func() {
 	})
 
 	AfterSuite(func() {
+		etcdRunner.Stop()
 		gexec.CleanupBuildArtifacts()
 	})
 
 	BeforeEach(func() {
 		etcdRunner.Start()
 
-		runner.Start(1*time.Second, taskKickInterval, 30*time.Minute, 30*time.Second, 300*time.Second)
-		Consistently(runner.Session.ExitCode).Should(Equal(-1))
-
 		var err error
 		var presenceStatus <-chan bool
-
 		fileServerPresence, presenceStatus, err = bbs.MaintainFileServerPresence(time.Second, "http://some.file.server", "file-server-id")
 		Ω(err).ShouldNot(HaveOccurred())
-
 		Eventually(presenceStatus).Should(Receive(BeTrue()))
+
+		executorPresence := models.ExecutorPresence{
+			ExecutorID: "the-executor-id",
+			Stack:      "the-stack",
+		}
+
+		etcdClient.Create(storeadapter.StoreNode{
+			Key:   shared.ExecutorSchemaPath(executorPresence.ExecutorID),
+			Value: executorPresence.ToJSON(),
+		})
+
 		test_helpers.NewStatusReporter(presenceStatus)
 	})
 
@@ -70,159 +84,241 @@ var _ = Describe("Main", func() {
 		etcdRunner.Stop()
 	})
 
-	Context("when a claimed task with a dead executor is present", func() {
-		JustBeforeEach(func() {
-			task := models.Task{
-				Domain: "tests",
+	startConverger := func() {
+		runner.Start(heartbeatInterval, taskKickInterval, 30*time.Minute, 30*time.Second, 300*time.Second)
+		time.Sleep(heartbeatInterval)
+	}
 
-				Guid:  "task-guid",
-				Stack: "stack",
-				Actions: []models.ExecutorAction{
-					{
-						Action: models.RunAction{
-							Path: "cat",
-							Args: []string{"/tmp/file"},
-						},
+	createClaimedTaskWithDeadExecutor := func() {
+		task := models.Task{
+			Domain: "tests",
+
+			Guid:  "task-guid",
+			Stack: "stack",
+			Actions: []models.ExecutorAction{
+				{
+					Action: models.RunAction{
+						Path: "cat",
+						Args: []string{"/tmp/file"},
 					},
 				},
-			}
+			},
+		}
 
-			err := bbs.DesireTask(task)
-			Ω(err).ShouldNot(HaveOccurred())
+		err := bbs.DesireTask(task)
+		Ω(err).ShouldNot(HaveOccurred())
 
-			err = bbs.ClaimTask(task.Guid, "dead-executor")
-			Ω(err).ShouldNot(HaveOccurred())
-		})
+		err = bbs.ClaimTask(task.Guid, "dead-executor")
+		Ω(err).ShouldNot(HaveOccurred())
+	}
 
-		It("eventually marks the task as failed", func() {
-			Eventually(bbs.GetAllCompletedTasks, taskKickInterval*2).Should(HaveLen(1))
-			tasks, err := bbs.GetAllTasks()
-			Ω(err).ShouldNot(HaveOccurred())
-			Ω(tasks).Should(HaveLen(1))
-			Ω(tasks[0].State).Should(Equal(models.TaskStateCompleted))
-			Ω(tasks[0].Failed).Should(BeTrue())
-		})
-	})
+	desireLRP := func() {
+		err := bbs.DesireLRP(models.DesiredLRP{
+			Domain: "tests",
 
-	Describe("when an LRP is desired", func() {
-		JustBeforeEach(func() {
-			err := bbs.DesireLRP(models.DesiredLRP{
-				Domain: "tests",
+			ProcessGuid: "the-guid",
 
-				ProcessGuid: "the-guid",
+			Stack: "some-stack",
 
-				Stack: "some-stack",
+			Instances: 3,
+			MemoryMB:  128,
+			DiskMB:    512,
 
-				Instances: 3,
-				MemoryMB:  128,
-				DiskMB:    512,
-
-				Actions: []models.ExecutorAction{
-					{
-						Action: models.RunAction{
-							Path: "the-start-command",
-						},
+			Actions: []models.ExecutorAction{
+				{
+					Action: models.RunAction{
+						Path: "the-start-command",
 					},
 				},
-			})
-			Ω(err).ShouldNot(HaveOccurred())
+			},
 		})
+		Ω(err).ShouldNot(HaveOccurred())
+	}
 
-		It("registers an app desire in etcd", func() {
-			Eventually(bbs.GetAllDesiredLRPs).Should(HaveLen(1))
-		})
+	itIsInactive := func() {
+		Describe("when an LRP is desired", func() {
+			JustBeforeEach(desireLRP)
 
-		Context("for an app that is not running at all", func() {
-			It("desires N start auctions in the BBS", func() {
-				Eventually(bbs.GetAllLRPStartAuctions, 0.5).Should(HaveLen(3))
-			})
-		})
-
-		Context("for an app that is missing instances", func() {
-			BeforeEach(func() {
-				bbs.ReportActualLRPAsRunning(models.ActualLRP{
-					ProcessGuid:  "the-guid",
-					InstanceGuid: "a",
-					Index:        0,
-				}, "executor-id")
-			})
-
-			It("start auctions for the missing instances", func() {
-				Eventually(bbs.GetAllLRPStartAuctions, 0.5).Should(HaveLen(2))
-				auctions, err := bbs.GetAllLRPStartAuctions()
-				Ω(err).ShouldNot(HaveOccurred())
-
-				indices := []int{auctions[0].Index, auctions[1].Index}
-				Ω(indices).Should(ContainElement(1))
-				Ω(indices).Should(ContainElement(2))
-
-				Consistently(bbs.GetAllLRPStartAuctions).Should(HaveLen(2))
-			})
-		})
-
-		Context("for an app that has extra instances", func() {
-			BeforeEach(func() {
-				bbs.ReportActualLRPAsRunning(models.ActualLRP{
-					ProcessGuid:  "the-guid",
-					InstanceGuid: "a",
-					Index:        0,
-				}, "executor-id")
-
-				bbs.ReportActualLRPAsRunning(models.ActualLRP{
-					ProcessGuid:  "the-guid",
-					InstanceGuid: "b",
-					Index:        1,
-				}, "executor-id")
-
-				bbs.ReportActualLRPAsRunning(models.ActualLRP{
-					ProcessGuid:  "the-guid",
-					InstanceGuid: "c",
-					Index:        2,
-				}, "executor-id")
-
-				bbs.ReportActualLRPAsRunning(models.ActualLRP{
-					ProcessGuid:  "the-guid",
-					InstanceGuid: "d-extra",
-					Index:        3,
-				}, "executor-id")
-			})
-
-			It("stops the extra instances", func() {
+			It("does not create start auctions for apps that are missing instances", func() {
 				Consistently(bbs.GetAllLRPStartAuctions, 0.5).Should(BeEmpty())
-				Eventually(bbs.GetAllStopLRPInstances).Should(HaveLen(1))
-				stopInstances, err := bbs.GetAllStopLRPInstances()
-				Ω(err).ShouldNot(HaveOccurred())
-
-				Ω(stopInstances[0].ProcessGuid).Should(Equal("the-guid"))
-				Ω(stopInstances[0].Index).Should(Equal(3))
-				Ω(stopInstances[0].InstanceGuid).Should(Equal("d-extra"))
 			})
 		})
 
-		Context("when an app is no longer desired", func() {
-			JustBeforeEach(func() {
-				Eventually(bbs.GetAllDesiredLRPs).Should(HaveLen(1))
-				err := bbs.RemoveDesiredLRPByProcessGuid("the-guid")
-				Ω(err).ShouldNot(HaveOccurred())
+		Context("when a claimed task with a dead executor is present", func() {
+			JustBeforeEach(createClaimedTaskWithDeadExecutor)
+
+			It("does not change the task", func() {
+				Consistently(bbs.GetAllCompletedTasks, taskKickInterval*2).Should(BeEmpty())
+			})
+		})
+	}
+
+	Context("when the converger has the lock", func() {
+		BeforeEach(startConverger)
+
+		Describe("when an LRP is desired", func() {
+			JustBeforeEach(desireLRP)
+
+			Context("for an app that is not running at all", func() {
+				It("desires N start auctions in the BBS", func() {
+					Eventually(bbs.GetAllLRPStartAuctions, 0.5).Should(HaveLen(3))
+				})
 			})
 
-			It("should remove the desired state from etcd", func() {
-				Eventually(bbs.GetAllDesiredLRPs).Should(HaveLen(0))
+			Context("for an app that is missing instances", func() {
+				BeforeEach(func() {
+					bbs.ReportActualLRPAsRunning(models.ActualLRP{
+						ProcessGuid:  "the-guid",
+						InstanceGuid: "a",
+						Index:        0,
+					}, "the-executor-id")
+				})
+
+				It("start auctions for the missing instances", func() {
+					Eventually(bbs.GetAllLRPStartAuctions, 0.5).Should(HaveLen(2))
+					auctions, err := bbs.GetAllLRPStartAuctions()
+					Ω(err).ShouldNot(HaveOccurred())
+
+					indices := []int{auctions[0].Index, auctions[1].Index}
+					Ω(indices).Should(ContainElement(1))
+					Ω(indices).Should(ContainElement(2))
+
+					Consistently(bbs.GetAllLRPStartAuctions).Should(HaveLen(2))
+				})
+			})
+
+			Context("for an app that has extra instances", func() {
+				BeforeEach(func() {
+					bbs.ReportActualLRPAsRunning(models.ActualLRP{
+						ProcessGuid:  "the-guid",
+						InstanceGuid: "a",
+						Index:        0,
+					}, "the-executor-id")
+
+					bbs.ReportActualLRPAsRunning(models.ActualLRP{
+						ProcessGuid:  "the-guid",
+						InstanceGuid: "b",
+						Index:        1,
+					}, "the-executor-id")
+
+					bbs.ReportActualLRPAsRunning(models.ActualLRP{
+						ProcessGuid:  "the-guid",
+						InstanceGuid: "c",
+						Index:        2,
+					}, "the-executor-id")
+
+					bbs.ReportActualLRPAsRunning(models.ActualLRP{
+						ProcessGuid:  "the-guid",
+						InstanceGuid: "d-extra",
+						Index:        3,
+					}, "the-executor-id")
+				})
+
+				It("stops the extra instances", func() {
+					Consistently(bbs.GetAllLRPStartAuctions, 0.5).Should(BeEmpty())
+					Eventually(bbs.GetAllStopLRPInstances).Should(HaveLen(1))
+					stopInstances, err := bbs.GetAllStopLRPInstances()
+					Ω(err).ShouldNot(HaveOccurred())
+
+					Ω(stopInstances[0].ProcessGuid).Should(Equal("the-guid"))
+					Ω(stopInstances[0].Index).Should(Equal(3))
+					Ω(stopInstances[0].InstanceGuid).Should(Equal("d-extra"))
+				})
+			})
+		})
+
+		Describe("when a claimed task with a dead executor is present", func() {
+			JustBeforeEach(createClaimedTaskWithDeadExecutor)
+
+			It("eventually marks the task as failed", func() {
+				Eventually(bbs.GetAllCompletedTasks, taskKickInterval*2).Should(HaveLen(1))
+				tasks, err := bbs.GetAllTasks()
+				Ω(err).ShouldNot(HaveOccurred())
+				Ω(tasks).Should(HaveLen(1))
+				Ω(tasks[0].State).Should(Equal(models.TaskStateCompleted))
+				Ω(tasks[0].Failed).Should(BeTrue())
 			})
 		})
 	})
 
-	Describe("then receives the SIGINT signal", func() {
-		It("exits successfully", func() {
-			runner.Session.Command.Process.Signal(syscall.SIGINT)
-			Eventually(runner.Session, 4).Should(gexec.Exit(0))
+	Context("when the converger loses the lock", func() {
+		BeforeEach(func() {
+			startConverger()
+			time.Sleep(heartbeatInterval)
+			err := etcdClient.Update(storeadapter.StoreNode{
+				Key:   shared.LockSchemaPath("converge_lock"),
+				Value: []byte("something-else"),
+			})
+			Ω(err).ShouldNot(HaveOccurred())
+
+			time.Sleep(heartbeatInterval + 10*time.Millisecond)
+		})
+
+		itIsInactive()
+
+		It("exits cleanly", func() {
+			Eventually(runner.Session.ExitCode).Should(Equal(0))
 		})
 	})
 
-	Describe("then receives the SIGTERM signal", func() {
-		It("exits successfully", func() {
-			runner.Session.Command.Process.Signal(syscall.SIGTERM)
-			Eventually(runner.Session, 4).Should(gexec.Exit(0))
+	Context("when the converger initially does not have the lock", func() {
+		BeforeEach(func() {
+			err := etcdClient.Create(storeadapter.StoreNode{
+				Key:   shared.LockSchemaPath("converge_lock"),
+				Value: []byte("something-else"),
+			})
+			Ω(err).ShouldNot(HaveOccurred())
+
+			startConverger()
+		})
+
+		itIsInactive()
+
+		Describe("when the lock becomes available", func() {
+			BeforeEach(func() {
+				err := etcdClient.Delete(shared.LockSchemaPath("converge_lock"))
+				Ω(err).ShouldNot(HaveOccurred())
+
+				time.Sleep(heartbeatInterval + 10*time.Millisecond)
+			})
+
+			Describe("when an LRP is desired", func() {
+				JustBeforeEach(desireLRP)
+
+				Context("for an app that is not running at all", func() {
+					It("desires N start auctions in the BBS", func() {
+						Eventually(bbs.GetAllLRPStartAuctions, 0.5).Should(HaveLen(3))
+					})
+				})
+			})
+
+			Describe("when a claimed task with a dead executor is present", func() {
+				JustBeforeEach(createClaimedTaskWithDeadExecutor)
+
+				It("eventually marks the task as failed", func() {
+					Eventually(bbs.GetAllCompletedTasks, taskKickInterval*2).Should(HaveLen(1))
+				})
+			})
+		})
+	})
+
+	Describe("signal handling", func() {
+		BeforeEach(func() {
+			startConverger()
+		})
+
+		Describe("when it receives SIGINT", func() {
+			It("exits successfully", func() {
+				runner.Session.Command.Process.Signal(syscall.SIGINT)
+				Eventually(runner.Session, 4).Should(gexec.Exit(0))
+			})
+		})
+
+		Describe("when it receives SIGTERM", func() {
+			It("exits successfully", func() {
+				runner.Session.Command.Process.Signal(syscall.SIGTERM)
+				Eventually(runner.Session, 4).Should(gexec.Exit(0))
+			})
 		})
 	})
 })
